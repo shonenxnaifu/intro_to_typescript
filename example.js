24e1c9ede3a81889f38fa4f00da83072e879f612
Pawitra Warda <pawitrawarda@gmail.com>
2022-10-23 02:28:56 +0700

    config file

---
+++ example.js
@@ -0,0 +1,8 @@
+//  Type Annotations
+let myName = "pw";
+let isCool = true;
+let favNumber = 32;
+function add(a, b) {
+    return a + b;
+}
+console.log(add(3, 2));
---
+++ example.ts
@@ -0,0 +1,11 @@
+//  Type Annotations
+let myName: string = "pw";
+let isCool: boolean = true;
+let favNumber: number = 32;
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+
+console.log(add(3,2));
